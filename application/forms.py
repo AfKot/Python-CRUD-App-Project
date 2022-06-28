81140b1865140c3759a264af580c896f2f9b5054
afzal <kotadia.afzal@gmail.com>
2022-06-28 12:20:34 +0000

    Updating Director forms

--- application/forms.py
+++ application/forms.py
@@ -5,5 +5,6 @@ from wtforms.validators import DataRequired, Length, ValidationError
 class MovieForm(FlaskForm):
     movie_name = StringField("Movie Title")
     rel_yr = IntegerField("Year of Release")
-    dir_name = StringField("Director")
     submit = SubmitField("Submit")
+
+class DirectorForm(FlaskForm):
