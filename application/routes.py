45c91c8955aabe7fd1b5d6a0b4a81c416b5523c5
afzal <kotadia.afzal@gmail.com>
2022-06-29 14:31:53 +0000

    added page to display no movies

--- application/routes.py
+++ application/routes.py
@@ -23,7 +23,7 @@ def dirMovs(id):
             movs.append(movies.movie_name)
             a = ", ".join(movs) 
         elif not movs:
-            return 'No Movies To Show'  
+            return render_template("noMovies.html")
     return render_template("directorsMovies.html", Directors=directors, Movies=a)
 
 @app.route('/addMovie', methods=['GET', 'POST'])
